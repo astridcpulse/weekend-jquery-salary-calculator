0c4be1c2630a6bec6f91e719045bbb59e7f49b1e
Astrid Pulse <astridcpulse@gmail.com>
2022-09-23 20:46:41 -0500

    linked salary.js to the DOM, created event handler function, created an addEmployee function that prevents default page loading by submit event

--- salary.js
+++ salary.js
@@ -1,1 +1,11 @@
-console.log('hey friend');
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('Ready Freddy!!!1!1!!!');
+
+    $('#employee-info-form').on('submit', addEmployee);
+}
+
+function addEmployee(event){
+    event.preventDefault();
+}
