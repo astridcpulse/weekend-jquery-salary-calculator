76e3bae1bcbab0d66ac8e09990df3f83118aabb9
Astrid Pulse <astridcpulse@gmail.com>
2022-09-23 20:54:02 -0500

    created employee object to store inputs. Created render function and tested the object being push to empty array

--- salary.js
+++ salary.js
@@ -1,5 +1,7 @@
 $(document).ready(onReady);
 
+let employeeArray = [];
+
 function onReady(){
     console.log('Ready Freddy!!!1!1!!!');
 
@@ -8,4 +10,22 @@ function onReady(){
 
 function addEmployee(event){
     event.preventDefault();
+
+    // new employee info entered in field
+    let newEmployee = {
+        firstName: $('#first-name').val(),
+        lastName: $('#last-name').val(),
+        employeeID: $('#employee-id').val(),
+        jobTitle: $('#job-title').val(),
+        annualSalary: $('#annual-salary').val()
+    }
+
+    // push new employee object to array
+    employeeArray.push(newEmployee);
+
+    render();
+}   
+
+function render(){
+    console.log('emp array', employeeArray);
 }
